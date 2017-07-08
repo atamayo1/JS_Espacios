0cdbac4476b8c9e60f97cdaec3a3209b3a096f3c
Anthony Tamayo Ortega <anthonytamayoortega@MacBook-Pro-de-Anthony.local>
2017-07-08 10:59:35 -0500

    Manejo de alerts y prompt terminado

---
+++ javaScript/espacios.js
@@ -0,0 +1,13 @@
+// Manejo de espacios en JavaScript
+var mensaje;
+mensaje = "Bienvenido al curso de \"JavaScript\"\n\nEsperamos que te diviertas ";
+var codPos = "54054";
+var clave = "";
+//Ejemplo de operador unario ++
+var a = 5;
+a++;
+//alert(mensaje);
+//Concatenar
+var nombre = prompt("¿Cuál es tu nombre?");
+var saludo = "Bienvenido ";
+alert(saludo+nombre);
